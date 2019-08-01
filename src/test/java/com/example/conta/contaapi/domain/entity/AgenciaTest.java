2c89d514fcf33941cf3d84fca738b377ed65b3f7
Gustavo Fatega <gus.fatega@gmail.com>
2019-08-01 16:13:23 -0300

    exercicio conta-api

---
+++ src/test/java/com/example/conta/contaapi/domain/entity/AgenciaTest.java
@@ -0,0 +1,5 @@
+package com.example.conta.contaapi.domain.entity;
+
+public class AgenciaTest {
+
+}
---
+++ src/test/java/com/example/conta/contaapiTest/EnderecoRepositoryTest.java
@@ -0,0 +1,5 @@
+package com.example.conta.contaapiTest;
+
+public class EnderecoRepositoryTest {
+
+}
